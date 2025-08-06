6aa5af6e6262e17a6e7e35847a4830bd05fa4554
Sergio Otalora <sergiootmi@gmail.com>
2025-08-06 13:09:42 +0200

    Version V0.0.1

---
+++ NansensuTopia/script.js
@@ -0,0 +1,17 @@
+//Maravilloso script àra añadir funcionalidad inútil a mi página.
+
+"use strict"; //Porque aquí somos muy estrictos...
+
+var nombre = prompt("Introduce tu nombre ", "nombre por defecto");
+
+document.writeln(nombre + " ha ingresado en El Círculo.")
+
+console.log("El usuario " + nombre + " " + "que ha entrado en esta página no sabe que el verdadero demonio es aquel que habita dentro de nosotros y que, en los momentos más oscuros de la noche, nos susurra palabras terribles y resplandecientes.")
+
+alert("¡Cuidado, hay demonios que viven en esta página!");
+
+let confirmacion = confirm("¿Estás segure de que quieres continuar por el camino de los demonios?");
+
+let number1 = 1
+let number2 = 0
+alert("¿Sabías que " + number1 + " + " + number2 + " es igual a 1?")
